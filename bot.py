30b92f7fe9bdbae6f11cef80a202666365fc3621
Austin Leath <32113014+AustinLeath@users.noreply.github.com>
2018-04-17 10:03:40 -0500

    Current version as of 4/17/18

--- bot.py
+++ bot.py
@@ -23,6 +23,16 @@ async def ping(ctx):
 
 
 @bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+        await bot.say("Your username is: " .format(user.name))
+        await bot.say("The users ID is: " .format(user.id))
+        await bot.say("The users status is: " .format(user.status))
+        await bot.say("The users highest role is: " .format(user.top_role))
+        await bot.say("The user specified joined this server at: ") .format(user.joined_at))
+        print("The user has issued the command !ping")
+
+
+@bot.command(pass_context=True)
 async def math(ctx):
         await bot.say("```YO I AM SO BLOCKED RN```")
         await bot.say(a + b + c)
