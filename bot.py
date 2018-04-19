d87d152f8ccd54dc4a631db4d242a47d3cc1343d
root <root@raspberrypi>
2018-04-19 13:58:18 -0500

    Current version as of 4/19/18

--- bot.py
+++ bot.py
@@ -2,27 +2,30 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
-import time
+
+path = 'token.txt'
+token_file = open(path,'r')
+token = token_file.read()
+tokenmain = token.strip()
 
 a = 2
 b = 2
 c = 2
 
-fh = open("token.txt","r")
-
 bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
+        print("Using the Token: " + tokenmain)
         print("BOT IS READY")
         print("BOT IS ONLINE WITH USERNAME: " + bot.user.name)
         while 1 == 1:
-            await bot.change_presence(game=discord.Game(name='type "!help"'))
-            time.sleep(15)
-            await bot.change_presence(game=discord.Game(name='type "!math"'))
-            time.sleep(15)
-            await bot.change_presence(game=discord.Game(name='type "!info"'))
-            time.sleep(15)
+            await bot.change_presence(game=discord.Game(name='type !help'))
+            await asyncio.sleep(20)
+            await bot.change_presence(game=discord.Game(name='type !math'))
+            await asyncio.sleep(20)
+            await bot.change_presence(game=discord.Game(name='type !about'))
+            await asyncio.sleep(20)
 
 @bot.command(pass_context=True)
 async def ping(ctx):
@@ -32,14 +35,12 @@ async def ping(ctx):
 
 @bot.command(pass_context=True)
 async def rcs(ctx):
-        embed = discord.Embed(title="Respone for !rcs", color=0x00fff00)
-        embed.add_field(name="Response", value="RILEIGH :heart:")
-        print("The user has issued the command !rcs")
-
+	await bot.say("RILEIGH :heart:")
+	print("The user has issued the command !rcs")
 @bot.command(pass_context=True)
 async def math(ctx):
         embed = discord.Embed(title="Respone for !math", color=0x00fff00)
-        embed.add_field(name="Description", value="his command adds three variables together which returns a sum", inline=True)
+        embed.add_field(name="Description", value="This command adds three variables together which returns a sum", inline=True)
         embed.add_field(name="Games you need to win", value=a + b + c, inline=True)
         embed.set_footer(text="v1.0.0")
         await bot.say(embed=embed)
@@ -65,4 +66,4 @@ async def info(ctx, user: discord.Member):
         await bot.say(embed=embed)
         print("The user has issued the command !info")
 
-bot.run(fh)
+bot.run(tokenmain)
