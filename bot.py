f039a244c5894cc4af67c44d2cd9cde117f1e5eb
AustinLeath <austinleath18@gmail.com>
2019-02-03 05:33:41 -0600

    Current version as of 2/3/19

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ path = 'token.txt'
 token_file = open(path,'r')
 token = token_file.read()
 tokenmain = token.strip()
-# below is a variable that stores the prefix in which the user inputs in Discord, for instance: "!ping", where the "!", symbolizes the usage of a command
+# below is a variable that stores the prefix in which A user inputs in Discord, for instance: "!ping", where the "!", symbolizes the usage of a command
 # for instance: "!ping", where the "!" in front of "ping" symbolizes the usage of a command which then captures the the string
 # that comes directly after "!" which then executes a command, if valid
 bot = commands.Bot(command_prefix="!")
@@ -20,7 +20,7 @@ async def on_ready():
     print("Using the Token: " + tokenmain)
     print("BOT IS READY WITH USER ID: " + bot.user.id)
     print("BOT IS ONLINE WITH USERNAME: " + bot.user.name)
-    while True:
+    while 1 == 1:
         await bot.change_presence(game=discord.Game(name='type !help'))
         await asyncio.sleep(20)
         await bot.change_presence(game=discord.Game(name='type !calc'))
@@ -42,11 +42,11 @@ async def on_ready():
 async def help(ctx):
     await bot.say('**!help** : displays this message')
     await bot.say('**!info** : displays information about a user')
-    await bot.say('**!about** : gives the user more information about the bot and the developer')
-    await bot.say('**!invite** : gives the user a link to invite the R6RC bot to their Discord')
-    await bot.say('**!app** : gives the user a link to the direct download for the R6RC desktop application')
-    await bot.say('**!twitter** : displays a dialogue directing the user to the R6RC Twitter account')
-    await bot.say('**!website** : gives the user a link to the online version of R6RC')
+    await bot.say('**!about** : gives A user more information about the bot and the developer')
+    await bot.say('**!invite** : gives A user a link to invite the R6RC bot to their Discord')
+    await bot.say('**!app** : gives A user a link to the direct download for the R6RC desktop application')
+    await bot.say('**!twitter** : displays a dialogue directing A user to the R6RC Twitter account')
+    await bot.say('**!website** : gives A user a link to the online version of R6RC')
     await bot.say('**!calc** : calculates user ranks')
     await bot.say('**!list** : displays all ranks with their associated MMR values')
     await bot.say('**!server** : displays an invite to the official R6RC server')
@@ -60,7 +60,7 @@ async def info(ctx, user: discord.Member):
     embed.add_field(name="Role", value="{}" .format(user.top_role), inline=True)
     embed.add_field(name="Joined", value="{}" .format(user.joined_at), inline=True)
     await bot.say(embed=embed)
-    print("The user has issued the command !info")
+    print("A user has issued the command !info")
 
 @bot.command(pass_context=True)
 async def about(ctx):
@@ -68,7 +68,7 @@ async def about(ctx):
     embed.add_field(name="Info", value="The developer of this bot is Austin Leath, he is a young web engineer looking for work. If you would like to contact him send him an email at austinleath18@gmail.com")
     embed.set_footer(text="v1.0.0 - @Game-King#0519")
     await bot.say(embed=embed)
-    print("The user has issued the command !about")
+    print("A user has issued the command !about")
 
 @bot.command(pass_context=True)
 async def invite(ctx):
@@ -76,7 +76,7 @@ async def invite(ctx):
     embed.add_field(name="Link", value="https://discordapp.com/api/oauth2/authorize?client_id=434432973362954241&permissions=8&scope=bot")
     embed.set_footer(text="v1.0.0 - @Game-King#0519")
     await bot.say(embed=embed)
-    print("The user has issued the command !invite")
+    print("A user has issued the command !invite")
 
 @bot.command(pass_context=True)
 async def app(ctx):
@@ -84,7 +84,7 @@ async def app(ctx):
     embed.add_field(name="Link", value="https://electronjs.org/apps/r6rc")
     embed.set_footer(text="v1.0.0 - @Game-King#0519")
     await bot.say(embed=embed)
-    print("The user has issued the command !app")
+    print("A user has issued the command !app")
 
 @bot.command(pass_context=True)
 async def twitter(ctx):
@@ -92,7 +92,7 @@ async def twitter(ctx):
     embed.add_field(name="Link", value="https://twitter.com/R6RankCalc")
     embed.set_footer(text="v1.0.0 - @Game-King#0519")
     await bot.say(embed=embed)
-    print("The user has issued the command !twitter")
+    print("A user has issued the command !twitter")
 
 @bot.command(pass_context=True)
 async def website(ctx):
@@ -100,7 +100,7 @@ async def website(ctx):
     embed.add_field(name="Link", value="https://r6rc.com/mmrcalculator.html")
     embed.set_footer(text="v1.0.0 - @Game-King#0519")
     await bot.say(embed=embed)
-    print("The user has issued the command !website")
+    print("A user has issued the command !website")
 
 @bot.command(pass_context=True)
 async def calc(ctx, arg1, arg2, arg3):
@@ -113,22 +113,31 @@ async def calc(ctx, arg1, arg2, arg3):
     SearchObjThree = re.search(r"[a-z]", goal, re.I)
 
     if(SearchObjOne):
-        await bot.say('Some of the characters that you entered were either not integers or negative, please try again')
-        print("The user has issued the command !calc with some characters that were either not integers or negative")
+        await bot.say('Some of the characters that you entered were not integers, please try again')
+        print("A user has issued the command !calc with some characters that were not integers")
     elif(SearchObjTwo):
-        await bot.say('Some of the characters that you entered were either not integers or negative, please try again')
-        print("The user has issued the command !calc with some characters that were either not integers or negative")
+        await bot.say('Some of the characters that you entered were not integers, please try again')
+        print("A user has issued the command !calc with some characters that were not integers")
     elif(SearchObjThree):
-        await bot.say('Some of the characters that you entered were either not integers or negative, please try again')
-        print("The user has issued the command !calc with some characters that were either not integers or negative")
+        await bot.say('Some of the characters that you entered were not integers, please try again')
+        print("A user has issued the command !calc with some characters that were not integers")
+    elif(int(mmr) < -10000 || int(mmr) > 10000):
+        await bot.say('Some of the numbers that you entered were either above the maximum or below the minimum value that R6RC supports, please try again')
+        print("A user has issued the command !calc with some characters that were either above the maximum or below the minimum value that R6RC supports")
+    elif(int(elo) < 1 || int(elo) > 500):
+        await bot.say('Some of the numbers that you entered were either above the maximum or below the minimum value that R6RC supports, please try again')
+        print("A user has issued the command !calc with some characters that were either above the maximum or below the minimum value that R6RC supports")
+    elif(int(goal) < -10000 || int(goal) > 10000):
+        await bot.say('Some of the numbers that you entered were either above the maximum or below the minimum value that R6RC supports, please try again')
+        print("A user has issued the command !calc with some characters that were either above the maximum or below the minimum value that R6RC supports")
     else:
-        mmr = math.fabs(int(arg1))
-        elo = math.fabs(int(arg2))
-        goal = math.fabs(int(arg3))
+        mmr = int(arg1))
+        elo = int(arg2))
+        goal = int(arg3))
 
         equation = (goal - mmr) / elo
         round = math.ceil(equation)
-        final = str(math.fabs(round))
+        final = str(int(math.fabs(round)))
 
         if(round < 0):
             winorlose = "You need to lose "
@@ -144,16 +153,16 @@ async def calc(ctx, arg1, arg2, arg3):
             matchcount = " matches "
         await bot.say('Your MMR is: ' + str(mmr) + ' your ELO is: ' + str(elo) + ' your goal is: ' + str(goal))
         await bot.say(winorlose + final + matchcount +'to reach your goal')
-        print("The user has issued the command !calc")
+        print("A user has issued the command !calc")
 
 @bot.command(pass_context=True)
 async def list(ctx):
     await bot.say('https://imgur.com/dc1REQz')
-    print("The user has issued the command !list")
+    print("A user has issued the command !list")
 
 @bot.command(pass_context=True)
 async def server(ctx):
     await bot.say('https://discord.gg/NaAmbbb')
-    print("The user has issued the command !server")
+    print("A user has issued the command !server")
 
 bot.run(tokenmain)
